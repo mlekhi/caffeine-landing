4ad02f73e0974242eb2e32c8bfc82e139522bbb7
Maya <maya.lekhi1@gmail.com>
2024-07-03 12:43:30 -0400

    fixing style

--- src/App.js
+++ src/App.js
@@ -31,33 +31,43 @@ function App() {
 
   return (
     <div className="App">
+      <div class='menu'>
+        <img src="logo512.png" className='menu-item'></img>
+        <p className='logo'>herbal</p>
+      </div>
       <header className="App-header">
         <div className="big-text">
           <h1>say goodbye to coffee chat jitters.</h1>
         </div>
-        <p>young professional? serial networker? we have you covered. </p>
-        <p>take your contact tracking system to the next level. automate your follow-ups and get insights on your networking.</p>
-        <p>turn your chats into 🍵</p>
+        <div>
+          <p>young professional? serial networker? we have you covered. </p>
+          <p>keep your connections warm by taking your networking tracker to the next level. plan your outreach efforts to meet your goals, automate your follow-ups, and get insights on your coffee chats.</p>
+        </div>
         <div>
           <form onSubmit={handleSubmit}>
            {!submitted && (
               <>
-                <label>
-                  Enter your email:
+                <p>
+                sign up to be one of the first users here: &emsp;
                   <input
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
-                  />
-                </label>
-                <button type="submit">Submit</button>
+                  /> &emsp;
+                  <button type="submit">Submit</button>
+                </p>
               </>
             )}
           </form>
           {submitted && <p>Thank you for subscribing!</p>}
         </div>
       </header>
+      <div class='footer'>
+        <p>made by &nbsp;</p>
+        <a href="https://github.com/mlekhi">maya</a>
+        <p>&nbsp; with 💚</p>
+      </div>
     </div>
   );
 }
